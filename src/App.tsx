ffe3d92b808ce0e586ea40195e734a3e1b416535
Guigalaverna <guigalaverna@gmail.com>
2021-07-06 18:04:47 -0300

    Using button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button } from "./components/Button";
 
 function Landing() {
   return (
@@ -13,25 +14,27 @@ function Landing() {
       </header>
 
       <main>
-        <section>
-          <div className="container">
-            <img
-              src="https://images.unsplash.com/photo-1562322140-8baeececf3df?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
-              alt="Mulher tratando do cabelo de outra mulher"
-            />
-            <h1>Saúde natural para os seus cabelos</h1>
-            <p>
-              Um salão exclusivo em São Paulo, especializado em tratamentos
-              naturais.
-            </p>
-            <a className="button" href="/#">
-              Agendar um horário
-            </a>
+        <section id="home" className="section">
+          <div className="container grid">
+            <div className="image">
+              <img
+                src="https://images.unsplash.com/photo-1562322140-8baeececf3df?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+                alt="Mulher tratando do cabelo de outra mulher"
+              />
+            </div>
+            <div className="text">
+              <h1>Saúde natural para os seus cabelos</h1>
+              <p>
+                Um salão exclusivo em São Paulo, especializado em tratamentos
+                naturais.
+              </p>
+              <Button href="/#">Agendar um horário</Button>
+            </div>
           </div>
         </section>
-        <section></section>
-        <section></section>
-        <section></section>
+        {/* <section className="section"></section>
+        <section className="section"></section>
+        <section className="section"></section> */}
       </main>
 
       <footer></footer>

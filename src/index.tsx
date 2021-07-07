05f4e5f2b3e6c7b84a2de4032923be26d16f7719
Guigalaverna <guigalaverna@gmail.com>
2021-07-07 03:09:01 -0300

    Using sidebar context provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { SidebarProvider } from "./contexts/SidebarContext";
 import "./styles/index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SidebarProvider>
+      <App />
+    </SidebarProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );

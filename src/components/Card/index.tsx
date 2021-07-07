76d92a395517da528c3c90264309a664dcde4c0f
Guigalaverna <guigalaverna@gmail.com>
2021-07-07 03:56:59 -0300

    Create card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { ReactSVG } from "react-svg";
+import "./styles.scss";
+
+interface CardProps {
+  title: string;
+  children: string;
+  icon: {
+    source: string;
+    alt: string;
+  };
+}
+
+export function Card({ title, children, icon }: CardProps) {
+  return (
+    <div className="card">
+      <ReactSVG src={icon.source} alt={icon.alt} />
+      <h3 className="title">{title}</h3>
+      <p>{children}</p>
+    </div>
+  );
+}

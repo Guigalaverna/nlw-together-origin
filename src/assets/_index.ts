a982d7aa8575055dbbd3e6ac69359993338f0853
Guigalaverna <guigalaverna@gmail.com>
2021-07-07 02:06:38 -0300

    Assets

---
+++ src/assets/_index.ts
@@ -0,0 +1,27 @@
+import Close from "./close.svg";
+import Cosmetic from "./cosmetic.svg";
+import Facebook from "./facebook.svg";
+import Instagram from "./instagram.svg";
+import Mail from "./mail.svg";
+import MapPing from "./map-pin.svg";
+import Menu from "./menu.svg";
+import Phone from "./phone.svg";
+import Trim from "./trim.svg";
+import WhatsApp from "./whatsapp.svg";
+import WomanHair from "./woman-hair.svg";
+import YouTube from "./youtube.svg";
+
+export {
+  Close,
+  Cosmetic,
+  Facebook,
+  Instagram,
+  Mail,
+  MapPing,
+  Menu,
+  Phone,
+  Trim,
+  WhatsApp,
+  WomanHair,
+  YouTube,
+};

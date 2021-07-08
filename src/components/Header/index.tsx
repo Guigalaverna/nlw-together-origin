b13a3632acd35d1ffc2a8442161fbfbebe520e5a
Guigalaverna <guigalaverna@gmail.com>
2021-07-08 17:27:42 -0300

    Removed useCallback import

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,4 @@
-import React from "react";
-import { useCallback } from "react";
-import { useRef } from "react";
-import { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { ReactSVG } from "react-svg";
 import { Menu } from "../../assets/_index";
 import { useSidebar } from "../../contexts/SidebarContext";
@@ -9,20 +6,20 @@ import "./styles.scss";
 
 export function Header() {
   const { handleToggleSidebarVisibility } = useSidebar();
-  const headerRef = useRef<any>()
-  
-  useEffect(() => {
-    const navHeight = headerRef.current?.offsetHeight
-    window.addEventListener('scroll', () => {
-      if (window.scrollY >= navHeight) {
-        headerRef.current?.classList.add('scroll')
-      } else {
-        headerRef.current?.classList.remove('scroll')
-      }
-    })
+  const headerRef = useRef<any>();
 
-    return () => window.removeEventListener('scroll', () => {})
-  }, [headerRef.current?.offsetHeight])
+  useEffect(() => {
+    const navHeight = headerRef.current?.offsetHeight;
+    window.addEventListener("scroll", () => {
+      if (window.scrollY >= navHeight) {
+        headerRef.current?.classList.add("scroll");
+      } else {
+        headerRef.current?.classList.remove("scroll");
+      }
+    });
+
+    return () => window.removeEventListener("scroll", () => {});
+  }, [headerRef.current?.offsetHeight]);
 
   return (
     <header ref={headerRef} className="header-container">

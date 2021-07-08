20ab8844f1a5c83a084d5388b09b5f7b45a896b7
Guigalaverna <guigalaverna@gmail.com>
2021-07-08 13:39:06 -0300

    Add height animation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { useCallback } from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
 import { ReactSVG } from "react-svg";
 import { Menu } from "../../assets/_index";
 import { useSidebar } from "../../contexts/SidebarContext";
@@ -6,9 +9,23 @@ import "./styles.scss";
 
 export function Header() {
   const { handleToggleSidebarVisibility } = useSidebar();
+  const headerRef = useRef<any>()
+  
+  useEffect(() => {
+    const navHeight = headerRef.current?.offsetHeight
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= navHeight) {
+        headerRef.current?.classList.add('scroll')
+      } else {
+        headerRef.current?.classList.remove('scroll')
+      }
+    })
+
+    return () => window.removeEventListener('scroll', () => {})
+  }, [headerRef.current?.offsetHeight])
 
   return (
-    <header className="header-container">
+    <header ref={headerRef} className="header-container">
       <nav className="container">
         <a className="logo" href="/#">
           beauty<span>salon</span>.
